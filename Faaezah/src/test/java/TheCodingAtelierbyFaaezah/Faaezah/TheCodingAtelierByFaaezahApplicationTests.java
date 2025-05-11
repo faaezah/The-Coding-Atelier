9371321b8645bea873aa389aa3d67c856f8e38a6
faaezah <faaezah.ahmed@gmail.com>
2025-05-11 16:43:23 -0500

    Alterations lmao

---
+++ Faaezah/src/test/java/TheCodingAtelierbyFaaezah/Faaezah/TheCodingAtelierByFaaezahApplicationTests.java
@@ -0,0 +1,13 @@
+package TheCodingAtelierbyFaaezah.Faaezah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCodingAtelierByFaaezahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
